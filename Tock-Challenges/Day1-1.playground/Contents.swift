88e44bd5ac30ea5b2f8da2b0b1ed7d8577c11833
Oliver Gane <ogane@rsgc.on.ca>
2016-02-01 08:11:09 -0500

    This is the real update I got to work. This was completed in Thursday’s class.

--- Tock-Challenges/Day1-1.playground/Contents.swift
+++ Tock-Challenges/Day1-1.playground/Contents.swift
@@ -2,4 +2,28 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+
+
+print("Welcome, please type in your day and month, we will tell you whether that is before or after February 18th")
+print("Please type in your day of the month")
+var day = Int(readLine(stripNewline: true)!)!
+print("Please enter in your month")
+var month = Int(readLine(stripNewline: true)!)!
+
+if  (day == 18){
+    if (month == 2) {
+        print("You entered in the correct date!")
+    }
+}
+
+if (month >= 2) {
+    if (day > 18) {
+        print("Before")
+    }
+}
+
+if (month <= 2) {
+    if (day < 18) {
+        print("After")
+    }
+}
