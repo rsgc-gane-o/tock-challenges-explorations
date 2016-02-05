22946abec9e281fb7067ca85ae7edc609fc97339
Oliver Gane <ogane@rsgc.on.ca>
2016-02-05 09:45:46 -0500

    Updated from last class

--- Tock-Challenges/Day3-1.playground/Contents.swift
+++ Tock-Challenges/Day3-1.playground/Contents.swift
@@ -3,3 +3,23 @@
 import Cocoa
 
 var str = "Hello, playground"
+let startingNumbers = (9*1)+(7*3)+(8*1)+(0*3)+(9*1)+(2*3)+(1*1)+(4*3)+(1*1)+(8*3)
+
+
+var digit1 = 9
+var digit2 = 4
+var digit3 = 8
+var input = (digit1*1) + (digit2*3) + (digit3*1)
+
+var output = input + startingNumbers
+
+if output%10 == 0 {
+    print("She works")
+}
+else {
+    
+        print("Nice try scammer")
+    }
+
+print("The 1-3-sum is:")
+print(output)
