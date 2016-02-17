f7df3ca9352b76e0c7fb2b9c0ae950ac5c5e835d
Oliver Gane <ogane@rsgc.on.ca>
2016-02-17 10:04:24 -0500

    Late update from last class. Working on fish challenge.

--- Tock-Challenges/Day3-2.playground/Contents.swift
+++ Tock-Challenges/Day3-2.playground/Contents.swift
@@ -3,3 +3,28 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+var trout = 1;
+var pike = 2;
+var pickerel = 3;
+var points = 2;
+var troutpoints = points/trout;
+var pikepoints = points/pike;
+var pickerelpoints = points/pickerel;
+
+
+if (trout <= points) {
+    if (trout % points == 0) {
+    print(troutpoints)
+    }
+}
+if (pike <= points) {
+    if (pike % points == 0){
+    print(pikepoints)
+    }
+}
+if (pickerel <= points) {
+    if (pickerel % points == 0 ){
+        print(pickerelpoints)
+    }
+}
